DysfunctionalROMs/vendor_broken

Add option for SystemMode SuperSU
Change-Id: I43ca28bf6860fba1074a3f3a1555762ec86ddf4b

config/common.mk
@@ -47,9 +47,15 @@ PRODUCT_COPY_FILES += \
     vendor/broken/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
 
 # SuperSU
+ifneq ($(NEEDS_SYSTEMMODE_SU),true)
+PRODUCT_COPY_FILES += \
+    vendor/broken/prebuilt/common/etc/SystemModeSuperSU.zip:system/addon.d/SuperSU.zip \
+    vendor/broken/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+else
 PRODUCT_COPY_FILES += \
     vendor/broken/prebuilt/common/etc/SuperSU.zip:system/addon.d/SuperSU.zip \
     vendor/broken/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+endif
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \

axxx007xxxz/zephyr_vendor_zos

common.mk
@@ -86,9 +86,15 @@ PRODUCT_BOOT_JARS += \
 
 
 # SuperSU
+ifneq ($(NEEDS_SYSTEMMODE_SU),true)
+PRODUCT_COPY_FILES += \
+    vendor/zos/prebuilt/common/etc/SystemModeSuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/zos/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+else
 PRODUCT_COPY_FILES += \
    vendor/zos/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
    vendor/zos/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+endif
 
 # Viper4Android
 PRODUCT_COPY_FILES += \