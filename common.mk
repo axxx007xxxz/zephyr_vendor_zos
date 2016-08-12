AospExtended/platform_vendor_aosp

vendor: copy init correctly
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

common.mk
@@ -52,7 +52,7 @@ PRODUCT_COPY_FILES += \
 
 # Init file
 PRODUCT_COPY_FILES += \
-    vendor/aosp/prebuilt/common/etc/init.local.rc:root/init.cm.rc
+    vendor/aosp/prebuilt/common/etc/init.local.rc:root/init.aosp.rc
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \

axxx007xxxz/zephyr_vendor_zos

Conflicts:
	common.mk
Change-Id: Ie41b0b5c320e015544f6a123b41c7eed9a0fad06

common.mk
@@ -70,7 +70,7 @@ PRODUCT_COPY_FILES += \
 
 # Init file
 PRODUCT_COPY_FILES += \
-    vendor/zos/prebuilt/common/etc/init.local.rc:root/init.cm.rc
+    vendor/zos/prebuilt/common/etc/init.local.rc:root/init.aosp.rc
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \