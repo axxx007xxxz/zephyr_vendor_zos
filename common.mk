AospExtended/platform_vendor_aosp

Add Substraum prebuilt

common.mk
@@ -63,6 +63,10 @@ PRODUCT_COPY_FILES +=  \
 PRODUCT_COPY_FILES += \
    vendor/aosp/prebuilt/common/etc/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
    vendor/aosp/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+
+# Substratum
+PRODUCT_COPY_FILES += \
+vendor/aosp/prebuilt/common/app/Substratum/Substratum.apk:system/app/Substratum/Substratum.apk
    
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \

axxx007xxxz/zephyr_vendor_zos

Conflicts:
	common.mk
Change-Id: I7092dbc52c6c51cb761498f0a56561b94ce1f160

common.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
    vendor/zos/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 endif
 
+# Substratum
+PRODUCT_COPY_FILES += \
+vendor/zos/prebuilt/common/app/Substratum/Substratum.apk:system/app/Substratum/Substratum.apk
+
 # Viper4Android
 PRODUCT_COPY_FILES += \
    vendor/zos/prebuilt/common/bin/audio_policy.sh:system/audio_policy.sh \