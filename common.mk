MapleAOSP/vendor-maple

Bringup system-squisher for Nougat (2/2)
Other commit is in build
Based on Cl3kener and FusionJacks Lollipop commits

config/common.mk
@@ -106,3 +106,6 @@ export ANDROID_JACK_VM_ARGS += "-Xmx8g"
 
 # Inherit common product build prop overrides
 -include vendor/maple/config/common_versions.mk
+
+# Squisher Location
+SQUISHER_SCRIPT := vendor/maple/tools/squisher

axxx007xxxz/zephyr_vendor_zos

Conflicts:
	config/common.mk

common.mk
@@ -166,3 +166,6 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 PRODUCT_PACKAGES += \
 	messaging \
         LiveWallpapersPicker
+
+# Squisher Location
+SQUISHER_SCRIPT := vendor/zos/tools/squisher